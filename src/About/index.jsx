44915fe5c41b2f58a99917b3e0e1387e9f49779a
Tran Thuy <thuytt0295@gmail.com>
2021-03-25 15:47:38 +0700

    add script

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat/lib/MessengerCustomerChat";
 
 class About extends Component {
+  // componentDidMount() {
+  //   window.fbAsyncInit = function () {
+  //     window.FB.init({
+  //       appId: "",
+  //       autoLogAppEvents: true,
+  //       xfbml: true,
+  //       version: "v3.0",
+  //     });
+  //   };
+  //   (function (d, s, id) {
+  //     const fjs = d.getElementsByTagName(s)[0];
+  //     if (d.getElementById(id)) return;
+  //     const js = d.createElement(s);
+  //     js.id = id;
+  //     js.src =
+  //       "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=1709228422490559&autoLogAppEvents=1";
+  //     fjs.parentNode.insertBefore(js, fjs);
+  //   })(document, "script", "facebook-jssdk");
+  //   window._sbzq ||
+  //     (function (e) {
+  //       e._sbzq = [];
+  //       const t = e._sbzq;
+  //       t.push(["_setAccount", 60187]);
+  //       const n = e.location.protocol === "https:" ? "https:" : "http:";
+  //       const r = document.createElement("script");
+  //       r.type = "text/javascript";
+  //       r.async = true;
+  //       r.src = n + "//static.subiz.com/public/js/loader.js";
+  //       const i = document.getElementsByTagName("script")[0];
+  //       i.parentNode.insertBefore(r, i);
+  //     })(window);
+  // }
   render() {
-    return <div>About page</div>;
+    console.log(window.location.pathname);
+    return (
+      <div>
+        About page
+        {/* <MessengerCustomerChat
+          pageId="107851557422909"
+          appId="1173105399778643"
+          htmlRef={window.location.pathname}
+        /> */}
+        ;
+      </div>
+    );
   }
 }
 
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -3,14 +3,16 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 
 class Home extends Component {
   render() {
+    console.log(window.location.pathname);
     return (
       <div>
         HOme page
-        <MessengerCustomerChat
+        {/* <MessengerCustomerChat
           pageId="107851557422909"
           appId="1173105399778643"
           htmlRef={window.location.pathname}
-        />
+        /> */}
+        <div class="fb-customerchat" page_id="107851557422909"></div>
       </div>
     );
   }
