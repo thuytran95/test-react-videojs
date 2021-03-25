fbc35067fb6e50dc35d3086e293fa5f74fb6d2d6
Tran Thuy <thuytt0295@gmail.com>
2021-03-25 12:56:23 +0700

    add all file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import videojs from "video.js";
+import VREPlayer from "videojs-react-enhanced";
+import "video.js/dist/video-js.css";
 
 function App() {
+  const playerOptions = {
+    src:
+      "https://file.mentor.vn/files/lessons/output/file-1614840748161/playlist.m3u8",
+    controls: true,
+    autoplay: "play",
+  };
+  const videojsOptions = {
+    fluid: true,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test video</h1>
+      <VREPlayer
+        playerOptions={playerOptions}
+        videojsOptions={videojsOptions}
+        onReady={(player) => console.log(player)}
+        onPlay={(e, _, second) => console.log("Play!")}
+        onPause={(e, _, second) => console.log("Pause!")}
+        onEnded={(e, _) => console.log("Ended!")}
+      />
     </div>
   );
 }
