ca8748c911800b8e8bf931554baca12087c80b94
Tran Thuy <thuytt0295@gmail.com>
2021-03-25 15:07:09 +0700

    add messanger in home

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 class Home extends Component {
   render() {
-    return <div>HOme page</div>;
+    return (
+      <div>
+        HOme page
+        <MessengerCustomerChat
+          pageId="107851557422909"
+          appId="1173105399778643"
+        />
+      </div>
+    );
   }
 }
 
