f0ef84ce3f4dadc7e3fc11df531d86e7050d6dec
Tran Thuy <thuytt0295@gmail.com>
2021-05-17 16:46:41 +0700

    css hidden control video in IPhone

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -9,7 +9,7 @@ class Home extends Component {
     return (
       <div>
         HOme page
-     <VideoPlayer url="https://file.mentor.vn/files/lessons/output/file-1619508998180/playlist.m3u8"/>
+     <VideoPlayer url="https://file.mentor.vn/files/lessons/file-1565945918422.mp4"/>
         {/* <div className="fb-customerchat" page_id="107851557422909"></div> */}
         <MessengerChat pageId="107851557422909" htmlRef="fb-msgr" />
       </div>
